b448e4b5de3391a5643e29c1cb7196b9b8e8ac18
dazzytech <dazzytech@hotmail.com>
2016-07-18 16:26:42 +0100

    Added Contract
    Contract will be transformed into the game parameters

---
+++ Artificer/Assets/Scripts/Space/Contract/ContractBuilder.cs
@@ -0,0 +1,425 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+// Artificer
+using Data.Shared;
+using Data.Shared.Missions;
+using Data.Space;
+using Space.Segment;
+
+namespace Space.Contract
+{
+    /*
+    public class ContractBuilder : MonoBehaviour
+    {
+        // Generator Objects
+        public ShipGenerator _shipGen;
+        public StationGenerator _stationGen;
+        public AsteroidGenerator _asteroidGen;
+        public SpawnPointGenerator _spawnGen;
+        public NodeGenerator _nodeGen;
+
+        // Use this for initialization
+        public void InitializeSpawners()
+        {
+            // Spawner attributes
+            //PlayerSpawner.Player = param.Player;
+            PlayerSpawner.ShipGen = _shipGen;
+            
+            // Generate asteroid data
+            AsteroidData aData = new AsteroidData();
+            aData.SetProspect("low"); // always low for now
+            aData.Type = "asteroid";
+            AsteroidSpawner.Asteroid = aData;
+            AsteroidSpawner.AstGen = _asteroidGen;
+            
+            FriendlySpawner.ShipGen = _shipGen;
+            EnemySpawner.ShipGen = _shipGen;
+        }
+
+        public void GenerateContractObjects(ContractData contract)
+        {
+            List<SpawnPointData> spawns = new List<SpawnPointData>();
+
+            // Design segment based on contract
+            switch (contract.CType)
+            {
+                case ContractType.BaseDefense:
+                    {
+                        Vector2 missionStation = 
+                        BuildStation(new Vector2(2500f, 2500f), contract).transform.position;
+
+                        // Build Player Spawns around the station
+                        int pSP = Random.Range(2, 5);
+                        for (int i = 0; i <= pSP; i++)
+                        {
+                            spawns.Add(BuildSpawn("playerSpawn", new Vector2
+                                              (Random.Range(missionStation.x - 50f, missionStation.x + 50f),
+                                                Random.Range(missionStation.y - 50f, missionStation.y + 50f))));
+                        }
+                    
+                        // Build asteroid spawners far from the station
+                        AsteroidSpawner.Target = missionStation;
+
+                        int aSP = Random.Range(5, 8);
+                        for (int i = 0; i <= aSP; i++)
+                        {
+                            bool outOfRange = false;
+                            Vector2 pos = new Vector2();
+                            while (!outOfRange)
+                            {
+                                pos = new Vector2
+                                (Random.Range(missionStation.x - 200f, missionStation.x + 200f),
+                                 Random.Range(missionStation.y - 200f, missionStation.y + 200f));
+                            
+                                if (Vector2.Distance(pos, missionStation) >= 100f && Vector2.Distance(pos, missionStation) < 200f)
+                                {
+                                    outOfRange = true;
+                                }
+                            }
+                        
+                            spawns.Add(BuildSpawn("asteroidSpawn", pos));
+                        }
+                    
+                        // Create friendly spawn points
+                        // Build Player Spawns around the station
+                        int fSP = Random.Range(2, 5);
+                        for (int i = 0; i <= fSP; i++)
+                        {
+                            spawns.Add(BuildSpawn("friendlySpawn", new Vector2
+                                       (Random.Range(missionStation.x - 50f, missionStation.x + 50f),
+                                        Random.Range(missionStation.y - 50f, missionStation.y + 50f))));
+                        }
+                    
+                        int eSP = Random.Range(4, 8);
+                        for (int i = 0; i <= eSP; i++)
+                        {
+                        
+                            bool outOfRange = false;
+                            Vector2 pos = new Vector2();
+                            while (!outOfRange)
+                            {
+                                pos = new Vector2
+                                (Random.Range(missionStation.x - 150f, missionStation.x + 150f),
+                                 Random.Range(missionStation.y - 150f, missionStation.y + 150f));
+                            
+                                if (Vector2.Distance(pos, missionStation) >= 75f && Vector2.Distance(pos, missionStation) < 150f)
+                                {
+                                    outOfRange = true;
+                                }
+                            }
+                            spawns.Add(BuildSpawn("enemySpawn", pos));
+                        }
+                    
+                        break;
+                    }
+                case ContractType.Attrition:
+                    {
+                        // Calculate two spawn points in the middle of segment
+                        // First point 
+                        Vector2 FriendlySpawn = new Vector2(2500, 2500);
+
+                        Vector2 EnemySpawn = Vector2.zero;
+
+                        bool outOfRange = false;
+                        while (!outOfRange)
+                        {
+                            EnemySpawn = new Vector2
+                            (Random.Range(FriendlySpawn.x - 100f, FriendlySpawn.x + 100f),
+                             Random.Range(FriendlySpawn.y - 100f, FriendlySpawn.y + 100f));
+                        
+                            if (Vector2.Distance(FriendlySpawn, EnemySpawn) >= 50f && Vector2.Distance(EnemySpawn, FriendlySpawn) < 100f)
+                            {
+                                outOfRange = true;
+                            }
+                        }
+
+                        // Build Player Spawns around the station
+                        int pSP = Random.Range(2, 5);
+                        for (int i = 0; i <= pSP; i++)
+                        {
+                            spawns.Add(BuildSpawn("playerSpawn", new Vector2
+                                              (Random.Range(FriendlySpawn.x - 50f, FriendlySpawn.x + 50f),
+                         Random.Range(FriendlySpawn.y - 50f, FriendlySpawn.y + 50f))));
+                        }
+
+                        // Create friendly spawn points
+                        // Build Player Spawns around the station
+                        int fSP = Random.Range(3, 6);
+                        for (int i = 0; i <= fSP; i++)
+                        {
+                            spawns.Add(BuildSpawn("friendlySpawn", new Vector2
+                                              (Random.Range(FriendlySpawn.x - 50f, FriendlySpawn.x + 50f),
+                         Random.Range(FriendlySpawn.y - 50f, FriendlySpawn.y + 50f))));
+                        }
+                    
+                        int eSP = Random.Range(4, 8);
+                        for (int i = 0; i <= eSP; i++)
+                        {
+                        
+                            spawns.Add(BuildSpawn("enemySpawn", new Vector2
+                                              (Random.Range(EnemySpawn.x - 50f, EnemySpawn.x + 50f),
+                            Random.Range(EnemySpawn.y - 50f, EnemySpawn.y + 50f))));
+                        }
+
+                        // Build asteroid spawners far from player
+                        int aSP = Random.Range(5, 8);
+                        for (int i = 0; i <= aSP; i++)
+                        {
+                            outOfRange = false;
+                            Vector2 pos = new Vector2();
+                            while (!outOfRange)
+                            {
+                                pos = new Vector2
+                                (Random.Range(FriendlySpawn.x - 200f, FriendlySpawn.x + 200f),
+                                 Random.Range(FriendlySpawn.y - 200f, FriendlySpawn.y + 200f));
+                                
+                            if (Vector2.Distance(pos, FriendlySpawn) >= 100f && Vector2.Distance(pos, FriendlySpawn) < 200f)
+                                {
+                                    outOfRange = true;
+                                }
+                            }
+                            
+                            spawns.Add(BuildSpawn("asteroidSpawn", pos));
+                        }
+                        break;
+                    }
+
+                case ContractType.Targets:
+                    {
+                        // Calculate two spawn points in the middle of segment
+                        // First point 
+                        Vector2 FriendlySpawn = new Vector2(2500, 2500);
+                        
+                        Vector2 EnemySpawn = Vector2.zero;
+                        
+                        bool outOfRange = false;
+                        while (!outOfRange)
+                        {
+                            EnemySpawn = new Vector2
+                                (Random.Range(FriendlySpawn.x - 100f, FriendlySpawn.x + 100f),
+                                 Random.Range(FriendlySpawn.y - 200f, FriendlySpawn.y + 100f));
+                            
+                            if (Vector2.Distance(FriendlySpawn, EnemySpawn) >= 50f && Vector2.Distance(EnemySpawn, FriendlySpawn) < 100f)
+                            {
+                                outOfRange = true;
+                            }
+                        }
+                        
+                        // Build Player Spawns around the station
+                        int pSP = Random.Range(2, 5);
+                        for (int i = 0; i <= pSP; i++)
+                        {
+                            spawns.Add(BuildSpawn("playerSpawn", new Vector2
+                                                  (Random.Range(FriendlySpawn.x - 50f, FriendlySpawn.x + 50f),
+                             Random.Range(FriendlySpawn.y - 50f, FriendlySpawn.y + 50f))));
+                        }
+                        
+                        // Create friendly spawn points
+                        // Build Player Spawns around the station
+                        int fSP = Random.Range(3, 6);
+                        for (int i = 0; i <= fSP; i++)
+                        {
+                            spawns.Add(BuildSpawn("friendlySpawn", new Vector2
+                                                  (Random.Range(FriendlySpawn.x - 50f, FriendlySpawn.x + 50f),
+                             Random.Range(FriendlySpawn.y - 50f, FriendlySpawn.y + 50f))));
+                        }
+                        
+                        int eSP = Random.Range(4, 8);
+                        for (int i = 0; i <= eSP; i++)
+                        {
+                            
+                            spawns.Add(BuildSpawn("enemySpawn", new Vector2
+                                                  (Random.Range(EnemySpawn.x - 50f, EnemySpawn.x + 50f),
+                             Random.Range(EnemySpawn.y - 50f, EnemySpawn.y + 50f))));
+                        }
+
+                        int aSP = Random.Range(5, 8);
+                        for (int i = 0; i <= aSP; i++)
+                        {
+                            outOfRange = false;
+                            Vector2 pos = new Vector2();
+                            while (!outOfRange)
+                            {
+                                pos = new Vector2
+                                    (Random.Range(FriendlySpawn.x - 200f, FriendlySpawn.x + 200f),
+                                     Random.Range(FriendlySpawn.y - 200f, FriendlySpawn.y + 200f));
+                                
+                                if (Vector2.Distance(pos, FriendlySpawn) >= 100f && Vector2.Distance(pos, FriendlySpawn) < 200f)
+                                {
+                                    outOfRange = true;
+                                }
+                            }
+                            
+                            spawns.Add(BuildSpawn("asteroidSpawn", pos));
+                        }
+                        break;
+                    }
+                case ContractType.Escort:
+                {
+                   // Create two stations with 100KM distance between them
+                    Vector2 homeStation = new Vector2(2500f, 2500f);
+                        //BuildStation(new Vector2(2500f, 2500f), contract, false).transform.position;
+
+                    Vector2 targetStation = new Vector2();
+                    bool outOfRange = false;
+                    while (!outOfRange)
+                    {
+                        targetStation = new Vector2
+                            (Random.Range(homeStation.x - 1000f, homeStation.x + 1000f),
+                             Random.Range(homeStation.y - 1000f, homeStation.y + 1000f));
+                        
+                        if (Vector2.Distance(homeStation, targetStation) >= 700f && Vector2.Distance(homeStation, targetStation) < 1000f)
+                        {
+                            outOfRange = true;
+                        }
+                    }
+
+                    BuildStation(targetStation, contract);
+
+                    // Create enemy and player spawns 
+                    // Build Player Spawns around the station
+                    // Follow So spawner has follow script
+                    int pSP = Random.Range(2, 5);
+                    for (int i = 0; i <= pSP; i++)
+                    {
+                        spawns.Add(BuildSpawn("playerSpawnFollow", new Vector2
+                                              (Random.Range(homeStation.x - 50f, homeStation.x + 50f),
+                         Random.Range(homeStation.y - 50f, homeStation.y + 50f))));
+                    }
+
+                    // Create friendly spawn points
+                    // Build Player Spawns around the station
+                    int fSP = Random.Range(2, 5);
+                    for (int i = 0; i <= fSP; i++)
+                    {
+                        spawns.Add(BuildSpawn("friendlySpawnFollow", new Vector2
+                                              (Random.Range(homeStation.x - 50f, homeStation.x + 50f),
+                         Random.Range(homeStation.y - 50f, homeStation.y + 50f))));
+                    }
+                    
+                    int eSP = Random.Range(4, 8);
+                    for (int i = 0; i <= eSP; i++)
+                    {
+                        
+                        outOfRange = false;
+                        Vector2 pos = new Vector2();
+                        while (!outOfRange)
+                        {
+                            pos = new Vector2
+                                (Random.Range(homeStation.x - 200f, homeStation.x + 200f),
+                                 Random.Range(homeStation.y - 200f, homeStation.y + 200f));
+                            
+                            if (Vector2.Distance(pos, homeStation) >= 100f && Vector2.Distance(pos, homeStation) < 200f)
+                            {
+                                outOfRange = true;
+                            }
+                        }
+                        spawns.Add(BuildSpawn("enemySpawnFollow", pos));
+                    }
+                    int aSP = Random.Range(5, 8);
+                    for (int i = 0; i <= aSP; i++)
+                    {
+                        outOfRange = false;
+                        Vector2 pos = new Vector2();
+                        while (!outOfRange)
+                        {
+                            pos = new Vector2
+                                (Random.Range(homeStation.x - 200f, homeStation.x + 200f),
+                                 Random.Range(homeStation.y - 200f, homeStation.y + 200f));
+                            
+                            if (Vector2.Distance(pos, homeStation) >= 100f && Vector2.Distance(pos, homeStation) < 200f)
+                            {
+                                outOfRange = true;
+                            }
+                        }
+                        
+                        spawns.Add(BuildSpawn("asteroidSpawnFollow", pos));
+                    }
+                    break;
+
+
+                }
+            }
+            
+            BuildSpawners(spawns.ToArray());
+        }
+    	
+        private void BuildSpawners(SpawnPointData[] spawns)
+        {
+            // Build one time segments
+            foreach(SpawnPointData spawn
+                    in spawns)
+            {
+                _spawnGen.GenerateSpawn(spawn);
+            }
+        }
+
+        /// <summary>
+        /// Builds the station.
+        /// in future this will be 
+        /// done in another class with defined vars
+        /// </summary>
+        /// <returns>The station.</returns>
+        private GameObject BuildStation(Vector2 position, ContractData contract)
+        {
+            // Build Station in middle of segment
+            // and set station as defense goal
+            StationData station = new StationData();
+            station.Position = position;
+            
+            station.SetAttributes("New Station", "Station_External_Small_01");
+            station.Type = "station";
+            
+            GameObject statObj = _stationGen.GenerateStation(station);
+
+            // Create nodes
+            _nodeGen.GenerateNodes(5, "PathNode", 
+                                   station.Position.x-25f, station.Position.y-25f, 50f, 50f
+                                   );
+            
+            // Set enemy target
+            switch(contract.CType)
+            {
+                case ContractType.BaseDefense:
+                {
+                    // Implement target bool if multiple stations created
+                    EnemySpawnManager.Target = statObj.transform;
+                    
+                    foreach (MissionData mission in contract.PrimaryMissions)
+                    {
+                        // in future do tests for right station
+                        if(mission is DefendMission) ((DefendMission)mission).Station =
+                            statObj.GetComponent<StationBehaviour>();
+                    }
+
+                    foreach (MissionData mission in contract.OptionalMissions)
+                    {
+                        // in future do tests for right station
+                        if(mission is DefendMission) ((DefendMission)mission).Station =
+                            statObj.GetComponent<StationBehaviour>();
+                    }
+                    break;
+                }
+                case ContractType.Escort:
+                {
+                        FriendlySpawnManager.Travel = statObj.transform;
+                    statObj.GetComponent<StationBehaviour>().Enter = true;
+                    break;
+                }
+            }
+
+            return statObj;
+        }
+
+        private SpawnPointData BuildSpawn(string type, Vector2 position)
+        {
+            SpawnPointData spawn = new SpawnPointData();
+            spawn.Position = position;
+            spawn.SpawnType = type;
+            
+            return spawn;
+        }
+    }*/
+}
+
---
+++ Artificer/Assets/Scripts/Space/Contract/ContractTracker.cs
@@ -0,0 +1,230 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Data.Shared;
+using Data.Shared.Missions;
+
+using Space.UI;
+
+/// <summary>
+/// Mission tracker.
+/// Set by the space manager and updates
+/// missions with the player update listener
+/// </summary>
+/// 
+namespace Space.Contract
+{
+    public enum ContractState{Pending, Completed, Failed}
+
+    public class ContractTracker: MonoBehaviour
+    {/*
+        ContractData Contract;
+        public ContractBuilder Builder;
+        public ContractState ContractStatus;
+        public RewardInfo Rewards;
+
+        // Mission Tracker List
+        public List<MissionData> PrimaryTracker;
+        public List<MissionData> SecondaryTracker;
+        public List<MissionData> Ended;
+
+        public void Initialize(GameParameters param)
+        {
+            // Initialize trackers
+            PrimaryTracker = new List<MissionData>();
+            SecondaryTracker = new List<MissionData>();
+            Ended = new List<MissionData>();
+
+            Contract = param.Contract;
+
+            ContractStatus = ContractState.Pending;
+
+            Builder.InitializeSpawners();
+
+            // Build space objects
+            Builder.GenerateContractObjects(Contract);
+
+            GameObject.Find("_gui").
+                SendMessage("BuildContractData", Contract);
+
+            foreach (MissionData mission in Contract.PrimaryMissions)
+            {
+                mission.Begin();
+                PrimaryTracker.Add(mission);
+            }
+
+            foreach (MissionData mission in Contract.OptionalMissions)
+            {
+                mission.Begin();
+                SecondaryTracker.Add(mission);
+            }
+        }
+
+        public void ProcessShipDestroyed(DestroyDespatch destroyed)
+        {
+            foreach (MissionData mission in PrimaryTracker)
+            {
+                mission.AddShipKilled(destroyed);
+            }
+            foreach (MissionData mission in SecondaryTracker)
+            {
+                mission.AddShipKilled(destroyed);
+            }
+        }
+
+        public void ProcessStationReached(Transform ship)
+        {
+            foreach (MissionData mission in PrimaryTracker)
+            {
+                mission.StationEntered(ship);
+            }
+            foreach (MissionData mission in SecondaryTracker)
+            {
+                mission.StationEntered(ship);
+            }
+        }
+
+        public void ProcessMaterials(Dictionary<MaterialData, float> newMat)
+        {
+            foreach (MissionData mission in PrimaryTracker)
+            {
+                mission.AddMaterial(newMat);
+            }
+            foreach (MissionData mission in SecondaryTracker)
+            {
+                mission.AddMaterial(newMat);
+            }
+        }
+
+        public void RunUpdate()
+        {
+            if (Contract != null)
+            {
+                bool CycleCompletion = true;
+                bool CycleFailure = false;
+
+                foreach (MissionData mission in PrimaryTracker)
+                {
+                    mission.UpdateStatus();
+                    if (!mission.Success)
+                    {
+                        CycleCompletion = false;
+                    }
+                    else
+                    {
+                        // Display success messege and remove item
+                        MessageHUD.DisplayMessege(new MsgParam("md-green", string.Format("Primary Mission Completed: {0}", mission.Title)));
+                        Ended.Add(mission);
+                    }
+                    if (mission.Failure)
+                    {
+                        CycleFailure = true;
+                        MessageHUD.DisplayMessege(new MsgParam("md-red", string.Format("Primary Mission Failed: {0}", mission.Title)));
+                    }
+                }
+
+                foreach (MissionData mission in SecondaryTracker)
+                {
+                    mission.UpdateStatus();
+                    if (mission.Success)
+                    {
+                        // Display success messege and remove item
+                        MessageHUD.DisplayMessege(new MsgParam("md-yellow", string.Format("Optional Mission Completed: {0}", mission.Title)));
+                        Ended.Add(mission);
+                    }
+                    if (mission.Failure)
+                    {
+                        MessageHUD.DisplayMessege(new MsgParam("md-yellow", string.Format("Optional Mission Failed: {0}", mission.Title)));
+                        Ended.Add(mission);
+                    }
+                }
+
+                foreach(MissionData mission in Ended)
+                {
+                    if(PrimaryTracker.Contains(mission))
+                        PrimaryTracker.Remove(mission);
+                }
+
+                foreach(MissionData mission in Ended)
+                {
+                    if(SecondaryTracker.Contains(mission))
+                        SecondaryTracker.Remove(mission);
+                }
+
+                if (CycleFailure)
+                    ContractStatus = ContractState.Failed;
+                else if (CycleCompletion)
+                {
+                    // Test that contract list exists
+                    /*if(GameManager.GetPlayer.Completedlevels == null)
+                        GameManager.GetPlayer.Completedlevels = new List<int>();
+
+                    // test contract is not already completed
+                    if(!GameManager.GetPlayer.Completedlevels.Contains(Contract.ID))
+                    {
+                        GameManager.GetPlayer.Completedlevels.Add(Contract.ID);
+                    }
+
+                    StoreRewards();
+
+                    // cycle through each ended mission and transfer rewards to player
+                    foreach(MissionData mission in Ended)
+                    {
+                        if(mission.Success)
+                        {
+                            if(mission.Reward != null)
+                            {
+                                // Transfer materials
+                                if(mission.Reward.Materials != null)
+                                    GameManager.GetPlayer.AddMaterial(mission.Reward.Materials);
+
+                                // Transfer components
+                                if(mission.Reward.Components != null)
+                                    GameManager.GetPlayer.AddComponent(mission.Reward.Components);
+
+                                GameManager.GetPlayer.AddXP(mission.Reward.Xp);
+                            }
+                        }
+                    }
+                    
+
+                    ContractStatus = ContractState.Completed;
+                }
+            }
+        }
+
+        public void StoreRewards()
+        {
+            Rewards = new RewardInfo();
+            // cycle through each ended mission and transfer rewards to player
+            foreach (MissionData mission in Ended)
+            {
+                if (mission.Success)
+                {
+                    if (mission.Reward != null)
+                    {
+                        Rewards.Xp += mission.Reward.Xp;
+
+                        foreach(MaterialData key in mission.Reward.Materials.Keys)
+                        {
+                            if(Rewards.Materials.ContainsKey(key))
+                                Rewards.Materials[key] += mission.Reward.Materials[key];
+                            else
+                                Rewards.Materials.Add(key, mission.Reward.Materials[key]);
+                        }
+
+                        foreach(string comp in mission.Reward.Components)
+                        {
+                            if(!Rewards.Components.Contains(comp))
+                            {
+                                Rewards.Components.Add(comp);
+                            }
+                        }
+                    }
+                }
+            }
+        }*/
+
+    }
+}
+
