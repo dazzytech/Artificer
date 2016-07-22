2f1255c64ff4dea437e1906b773a83ad3f7669a1
dazzytech <dazzytech@hotmail.com>
2016-07-22 14:12:46 +0100

    Started TrackerHUD update
    had to stop due to lag, finish at home

--- Artificer/Assets/Scripts/Space/UI/ShipHUD/TrackerHUD.cs
+++ Artificer/Assets/Scripts/Space/UI/ShipHUD/TrackerHUD.cs
@@ -9,6 +9,13 @@ using Space.Ship;
 
 namespace Space.UI.Tracker
 {
+
+    #region MARKER CONTAINER
+
+    /// <summary>
+    /// Contaier class for tracking information 
+    /// regarding transforms
+    /// </summary>
     public class Marker
     {
         public Transform trackedObj;
@@ -18,14 +25,25 @@ namespace Space.UI.Tracker
         public float trackDist;
     }
 
-    public class TrackerHUD : MonoBehaviour {
-    	Transform playerTransform; 
+    #endregion
 
-        private ContractData _contract;
-        private List<Transform> _targets;
+    /// <summary>
+    /// Tracks nearby transforms using tags
+    /// to identify and displays in circle
+    /// </summary>
+    public class TrackerHUD : MonoBehaviour
+    {
+        //private ContractData _contract;
+        //private List<Transform> _targets;
+
+        #region MARKER LISTS
 
         List<Marker> _markers;
         List<Marker> _pendingDelete;
+
+        #endregion
+
+        #region UI HUD PREFABS
 
         public GameObject undefinedArrow;
         public GameObject undefinedBox;
@@ -37,35 +55,38 @@ namespace Space.UI.Tracker
         public GameObject friendlyBox;
         public GameObject missionArrow;
         public GameObject missionBox;
+
+        #endregion
+
+        #region MISC ATTRIBUTES
+
+        // Font used to display distance text
         public Font font;
 
-    	float _radius = 100f;
+        // radius of the UI circle
+        float _radius = 100f;
+
+        #endregion
+
+        #region MONO BEHAVIOUR
 
         void Awake()
         {
             _markers = new List<Marker>();
             _pendingDelete = new List<Marker>();
-            _targets = new List<Transform>();
+            //_targets = new List<Transform>();
         }
 
-        public void SetContactData(ContractData contract)
+        /*public void SetContactData(ContractData contract)
         {
-            _contract = contract;
-        }
-
-        void Start()
-        {
-        }
+            //_contract = contract;
+        }*/
 
         public void AddUIPiece(Transform piece)
         {
-            if (GameObject.FindGameObjectWithTag 
+            if (GameObject.FindGameObjectWithTag
                 ("MainCamera") == null)
                 return;
-
-            playerTransform = 
-                GameObject.FindGameObjectWithTag 
-                    ("MainCamera").transform;
 
             // stations aren't added this way so piece is a ship
             Marker m = new Marker();
@@ -74,78 +95,83 @@ namespace Space.UI.Tracker
             _markers.Add(m);
         }
 
-    	
-    	// Update is called once per frame
-    	void LateUpdate () 
+
+        void LateUpdate()
         {
-            /*playerTransform = 
-                GameObject.FindGameObjectWithTag 
+            // retreive the position of the main camera in our scene
+            Transform cameraTransform =
+                GameObject.FindGameObjectWithTag
                     ("MainCamera").transform;
 
-    		if (playerTransform != null && _markers != null) 
+            // If we don't have a camera or anything to track
+            // this stops here
+            if (cameraTransform == null || _markers == null)
+                return;
+
+            int index = 0;
+
+            foreach (Marker m in _markers)
             {
-                int index = 0;
-
-                foreach(Marker m in _markers)
+                if (m.trackedObj == null)
                 {
-                    if(m.trackedObj == null)
-                    {
-                        _pendingDelete.Add(m);
-                        if(m.arrow != null)m.arrow.SetActive(false);
-                        if(m.box != null)m.box.SetActive(false);
-                        if(m.text != null)m.text.SetActive(false);
-                        continue;
-                    }
-
-                    if(m.arrow == null || m.box == null
-                       || m.text == null)
-                        BuildGUIPiece(m);
-
-                    float objDistance = SetPieceVisiblity(m);
-
-                    Vector2 dir = 
-                        (m.trackedObj.position - playerTransform.position)
-                            .normalized * _radius;
-                    m.arrow.transform.up = dir.normalized;
-                    m.arrow.transform.localPosition = dir;
-
-                    
-                    //now you can set the position of the ui element
-                    m.box.GetComponent<RectTransform>().anchoredPosition=
-                        UIConvert.WorldToCamera(m.trackedObj);
-                    m.text.transform.localPosition = dir + dir.normalized*Overlap(index, m);
-                    m.text.GetComponent<Text>().text = ((int)objDistance/10).ToString()+"km";
-
-                    index++;
+                    _pendingDelete.Add(m);
+                    if (m.arrow != null) m.arrow.SetActive(false);
+                    if (m.box != null) m.box.SetActive(false);
+                    if (m.text != null) m.text.SetActive(false);
+                    continue;
                 }
 
-                foreach(Marker m in _pendingDelete)
-                {
-                    _markers.Remove(m);
-                }
-    		}*/
-    	}
+                if (m.arrow == null || m.box == null
+                   || m.text == null)
+                    BuildGUIPiece(m);
 
-        private int Overlap(int index, Marker m)
+                float objDistance = SetPieceVisiblity(m, cameraTransform.position);
+
+                Vector2 dir =
+                    (m.trackedObj.position - cameraTransform.position)
+                        .normalized * _radius;
+                m.arrow.transform.up = dir.normalized;
+                m.arrow.transform.localPosition = dir;
+
+
+                //now you can set the position of the ui element
+                m.box.GetComponent<RectTransform>().anchoredPosition =
+                    UIConvert.WorldToCamera(m.trackedObj);
+                m.text.transform.localPosition = dir + dir.normalized * Overlap(index, m, cameraTransform.position);
+                m.text.GetComponent<Text>().text = ((int)objDistance / 10).ToString() + "km";
+
+                index++;
+            }
+
+            foreach (Marker m in _pendingDelete)
+            {
+                _markers.Remove(m);
+            }
+        }
+
+
+        #endregion
+
+        private int Overlap(int index, Marker m, Vector3 cameraPosition)
         {
             int overlap = 30;
 
-            Vector2 dirA = 
-                (m.trackedObj.position - playerTransform.position);
+            Vector2 dirA =
+                (m.trackedObj.position - cameraPosition);
 
             for (; index > 0;)
             {
                 Marker testM = _markers[--index];
-                if(testM.trackedObj == null)
+                if (testM.trackedObj == null)
                     continue;
                 else if (!testM.arrow.activeSelf)
                     continue;
 
-                Vector2 dirB = 
-                    (testM.trackedObj.position - playerTransform.position);
+                Vector2 dirB =
+                    (testM.trackedObj.position - cameraPosition);
 
                 float angle = Vector2.Angle(dirA, dirB);
-                if(angle < 10f)
+                if (angle < 10f)
                 {
                     overlap += 30;
                 }
@@ -158,7 +184,7 @@ namespace Space.UI.Tracker
         {
             foreach (Marker m in _markers)
             {
-                if(m.trackedObj.tag != "Station")
+                if (m.trackedObj.tag != "Station")
                 {
                     Destroy(m.arrow);
                     m.arrow = null;
@@ -175,12 +201,11 @@ namespace Space.UI.Tracker
         /// </summary>
         /// <returns>The piece distance to the object.</returns>
         /// <param name="m">M.</param>
-        private float SetPieceVisiblity(Marker m)
+        private float SetPieceVisiblity(Marker m, Vector3 camPos)
         {
-            float objDistance = Vector3.Distance(m.trackedObj.position,
-                                                 playerTransform.position);
+            float objDistance = Vector3.Distance(m.trackedObj.position, camPos);
             Bounds camB = CameraExtensions.OrthographicBounds(Camera.main);
-            if(camB.Contains(m.trackedObj.position))
+            if (camB.Contains(m.trackedObj.position))
             {
                 m.arrow.SetActive(false);
                 m.box.SetActive(true);
@@ -188,7 +213,7 @@ namespace Space.UI.Tracker
             }
             else
             {
-                if(m.trackDist == -1 || objDistance <= m.trackDist)
+                if (m.trackDist == -1 || objDistance <= m.trackDist)
                 {
                     m.arrow.SetActive(true);
                     m.text.SetActive(true);
@@ -208,7 +233,7 @@ namespace Space.UI.Tracker
         {
             if (m.trackedObj.tag == "Station")
             {
-                Vector2 dir = (m.trackedObj.position 
+                Vector2 dir = (m.trackedObj.position
                     - playerTransform.position).normalized * _radius;
 
                 m.text = new GameObject();
@@ -217,7 +242,7 @@ namespace Space.UI.Tracker
                 mtext.font = font;
                 mtext.alignment = TextAnchor.MiddleCenter;
                 mtext.fontSize = 8;
-                    
+
                 m.arrow = (GameObject)Instantiate(stationArrow, dir, Quaternion.identity);
                 m.arrow.transform.SetParent(this.transform, false);
                 m.box = (GameObject)Instantiate(stationBox, dir, Quaternion.identity);
@@ -229,7 +254,7 @@ namespace Space.UI.Tracker
             {
                 m.trackDist = 500f;
 
-                Vector2 dir = (m.trackedObj.position 
+                Vector2 dir = (m.trackedObj.position
                                    - playerTransform.position).normalized * _radius;
                 m.text = new GameObject();
                 Text text = m.text.AddComponent<Text>();
@@ -240,7 +265,7 @@ namespace Space.UI.Tracker
 
                 // add different arrow depending
                 string relation = m.trackedObj.name;
-                
+
                 switch (relation)
                 {
                     case "Friendly":
@@ -298,9 +323,25 @@ namespace Space.UI.Tracker
                         m.box.SetActive(false);
                         m.arrow.SetActive(false);
                         text.color = Color.red;
-                    break;
+                        break;
                 }
             }
+
+
         }
+
+        #region MARKER UTILITIES
+
+        /// <summary>
+        /// Clears away all hud elements of the marker
+        /// and adds it to the pending deleted
+        /// </summary>
+        /// <param name="m"></param>
+        private void DeleteMarker(Marker m)
+        {
+
+        }
+
+        #endregion
     }
 }
