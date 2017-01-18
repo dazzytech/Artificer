a7cbe518c0cfb5d6529dd4463178c90c790e71c4
Dazzytech <dazzytech@hotmail.com>
2017-01-18 08:47:55 +0000

    update stationHud

--- Artificer/Assets/Scripts/Space/UI/PrefabScripts/Station/IntegrityTracker.cs
+++ Artificer/Assets/Scripts/Space/UI/PrefabScripts/Station/IntegrityTracker.cs
@@ -16,7 +16,23 @@ namespace Space.UI.Ship
         [SerializeField]
         public Image[] ImageList;
 
+        [SerializeField]
+        private bool Recolor;
+
+        private Color HighColor;
+
+        [SerializeField]
+        private Color MedColor;
+
+        [SerializeField]
+        private Color LowColor;
+
         #endregion
+
+        void Awake()
+        {
+            HighColor = ImageList[0].color;
+        }
 
         #region PUBLIC INTERACTION
 
@@ -27,11 +43,24 @@ namespace Space.UI.Ship
         /// <param name="normalValue"></param>
         public void Step(float normalValue)
         {
+            if(Recolor)
+            {
+                if (normalValue <= 1f)
+                    foreach (Image segment in ImageList)
+                        segment.color = HighColor;
+                if (normalValue <= 0.6f)
+                    foreach (Image segment in ImageList)
+                        segment.color = MedColor;
+                if (normalValue <= 0.3f)
+                    foreach (Image segment in ImageList)
+                        segment.color = LowColor;
+            }
+
             // Simply go down in increments showing and hiding each segment
-            for(int i = 0; i < 10; i++)
+            for(int i = 0, a = ImageList.Length-1; i < ImageList.Length; i++, a--)
             {
                 // 1.0 - (1 * 0.1f) = 0.9
-                CompareValue(normalValue, (1.0f - (i+1) * 0.1f), ImageList[i]);
+                CompareValue(normalValue, (1.0f - (i+1) * 0.1f), ImageList[a]);
             }
         }
 
@@ -48,9 +77,9 @@ namespace Space.UI.Ship
         private void CompareValue(float normalValue, float compare, Image segment)
         {
             if (normalValue <= compare)
-                segment.enabled = false;
+                segment.CrossFadeAlpha(0f, 0f, true);
             else
-                segment.enabled = true;
+                segment.CrossFadeAlpha(1f, 0f, true);
         }
 
         #endregion
