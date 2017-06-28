b2b8c645cce2b8c82a9845010e313e53254c507d
Dazzytech <dazzytech@hotmail.com>
2017-06-28 17:12:17 +0100

    begin authority spawn

--- Artificer/Assets/Scripts/Space/Ships/ShipInitializer.cs
+++ Artificer/Assets/Scripts/Space/Ships/ShipInitializer.cs
@@ -9,11 +9,16 @@ using Data.Space.Library;
 using Space.Segment.Generator;
 using Space.UI;
 using Networking;
+using System.Collections.Generic;
+using Space.Ship.Components.Listener;
+using Space.Ship.Components.Attributes;
 
 namespace Space.Ship
 {
     /// <summary>
-    /// When a ship is initialized then 
+    /// Builds the ship object
+    /// upon creation following 
+    /// criteria
     /// </summary>
     public class ShipInitializer : NetworkBehaviour
     {
@@ -34,6 +39,19 @@ namespace Space.Ship
         public string Ship;
         [SyncVar]
         public bool hasSpawned;
+
+        #region GENERATION VARIABLES
+
+        private List<int> m_addedIDs;
+
+        #endregion
+
+        #region CONTAINER
+
+        [Header("Container")]
+        private Transform m_shipContainer;
+
+        #endregion
 
         #endregion
 
@@ -56,6 +74,8 @@ namespace Space.Ship
                 if (OnShipCreated != null)
                     OnShipCreated(ShipAtt);
             }
+
+            transform.SetParent(m_shipContainer);
         }
 
         #endregion
@@ -64,7 +84,9 @@ namespace Space.Ship
 
         void Awake()
         {
-            NetworkManager.singleton.client.RegisterHandler((short)MSGCHANNEL.SPAWNME, OnSpawnMe);
+            // Retrive container object
+            m_shipContainer = 
+                GameObject.Find("_ships").transform;
         }
 
         #endregion
@@ -184,6 +206,217 @@ namespace Space.Ship
             if (OnShipCreated != null)
                 OnShipCreated(ShipAtt);
         }
+
+        #endregion
+
+        #region PRIVATE UTILITIES
+
+        #region SHIP GENERATION
+
+        /// <summary>
+        /// When Attributes have been assigned then
+        /// this command with generate the ship
+        /// </summary>
+        /// <returns>The AI ship.</returns>
+        /// <param name="ship">ShipData template 
+        /// to ship the ship with.</param>
+        [Command]
+        public void CmdGenerateShip
+            (ShipData ship)
+        {
+            // create list for storing info
+            m_addedIDs = new List<int>();
+
+            // Add head to this game object
+            GameObject headGO =
+                Instantiate(Resources.Load
+                            ("Space/Ships/" + ship.Head.Path))
+                    as GameObject;
+
+            // set transform
+            headGO.transform.parent = transform;
+            headGO.transform.localPosition = Vector3.zero;
+            headGO.tag = "Head";
+            headGO.SendMessage("SetStyle", ship.Head.Style,
+                               SendMessageOptions.DontRequireReceiver);
+            headGO.SendMessage("SetID", ship.Head.InstanceID,
+                SendMessageOptions.DontRequireReceiver);
+
+            m_addedIDs.Add(ship.Head.InstanceID);
+
+            // build the body around this
+            ShipGenerator.BuildConnectedPieces
+                (ship.Head, headGO.transform, ship);
+
+            //baseShip.GetComponent<ShipAttributes>().Ship = ship;
+            SendMessage("AddComponentsToList",
+                SendMessageOptions.DontRequireReceiver);
+        }
+
+        /// <summary>
+        /// Builds the connected pieces.
+        /// Takes a piece and constructs all 
+        /// connected pieces and 
+        /// works recursively
+        /// </summary>
+        /// <param name="component">Component.</param>
+        /// <param name="componentTransform">Component transform.</param>
+        /// <param name="ship">Ship.</param>
+        [Server]
+        public void BuildConnectedPieces
+            (Data.Shared.Component component, Transform componentTransform,
+             ShipData ship)
+        {
+            Vector3 otherPos;
+            Vector3 thisPos;
+
+            Socket[] socketList = component.sockets;
+
+            // no sockets = no body
+            if (socketList == null)
+                return;
+
+            foreach (Socket socket in socketList)
+            {
+                // Get position of this socket
+                // through the components transform
+                Transform thisTrans = componentTransform.Find
+                    (String.Format
+                     ("socket_{0}", socket.SocketID));
+
+                // test we successfully found the socket 
+                if (thisTrans == null)
+                {
+                    Debug.Log("Ship Generator - " +
+                            "BuildConnectedPieces:" +
+                              "socket Transform not found - "
+                              + socket.SocketID);
+                    return;
+                }
+
+                thisPos =
+                    componentTransform.Find
+                        (String.Format
+                         ("socket_{0}", socket.SocketID)).position;
+
+                // find the second piece through the socket
+                Data.Shared.Component piece = ship.GetComponent(socket.OtherID);
+                if (piece.Path == "")
+                {
+                    Debug.Log("Ship Generator - " +
+                        "BuildConnectedPieces: other " +
+                        "socket not found!");
+                    return;
+                }
+
+                // test we haven't already added this piece.
+                // stops unending loops
+                if (m_addedIDs.Contains(piece.InstanceID))
+                    continue;
+
+                // create the piece
+                GameObject pieceGO = null;
+
+                // Test if we actually successfully created the new piece
+                if (Resources.Load
+                    ("Space/Ships/" + piece.Path) == null)
+                {
+                    Debug.Log("Ship Generator - " +
+                              "BuildConnectedPieces: other " +
+                              "Could not find: " + piece.Path);
+                    return;
+                }
+
+                //Add the component piece to the game world
+                pieceGO =
+                    Instantiate(Resources.Load
+                                ("Space/Ships/" + piece.Path))
+                            as GameObject;
+
+                // Set the direction of the new piece
+                Vector3 dirEuler = new Vector3(0, 0, 0);
+                switch (piece.Direction)
+                {
+                    case "up":
+                        dirEuler.z = 0f; break;
+                    case "down":
+                        dirEuler.z = 180f; break;
+                    case "left":
+                        dirEuler.z = 90; break;
+                    case "right":
+                        dirEuler.z = 270f; break;
+                }
+
+                // Apply direction to obj and sockets
+                pieceGO.transform.eulerAngles = dirEuler;
+
+                // Set trigger - none interactive components will ignore this
+                pieceGO.SendMessage("SetTriggerKey", piece.Trigger,
+                                    SendMessageOptions.DontRequireReceiver);
+
+                // Combat trigger - activation key when the ship enters combat mode
+                if (piece.CTrigger != null)
+                    pieceGO.SendMessage("SetCombatKey", piece.CTrigger,
+                                    SendMessageOptions.DontRequireReceiver);
+
+                // Sets the ship's visual style
+                pieceGO.SendMessage("SetStyle", piece.Style,
+                                    SendMessageOptions.DontRequireReceiver);
+
+                pieceGO.SendMessage("SetID", piece.InstanceID,
+                                    SendMessageOptions.DontRequireReceiver);
+
+                // make child of ship
+                pieceGO.transform.parent =
+                    componentTransform.parent;
+
+                // find position of other piece and then
+                // snap the pieces together
+                otherPos = pieceGO.transform.Find
+                    (String.Format
+                     ("socket_{0}", socket.OtherLinkID)).position;
+
+                Vector3 snapDistance = otherPos - thisPos;
+
+                pieceGO.transform.position -= snapDistance;
+
+                // Initiailize the connector list for this piece
+                pieceGO.SendMessage("InitCL");
+
+                pieceGO.SendMessage("SetSock", socket);
+
+                pieceGO.SendMessage("LockTo", componentTransform);
+
+                // Add this piece to the parent piece connector list
+                componentTransform.gameObject.SendMessage
+                    ("AddConnection", pieceGO.GetComponent<ComponentListener>());
+
+                // Finally assign autolock and autofire if component is the right type
+                if (piece.Folder == "Launchers")
+                {
+                    pieceGO.GetComponent<LauncherAttributes>().AutoTarget =
+                        piece.AutoLock;
+                }
+
+                if (piece.Folder == "Targeter")
+                {
+                    pieceGO.GetComponent<TargeterAttributes>().Behaviour =
+                        (TargeterBehaviour)piece.behaviour;
+
+                    pieceGO.GetComponent<TargeterAttributes>().EngageFire =
+                        piece.AutoFire;
+
+                    pieceGO.GetComponent<TargeterAttributes>().homeForward = pieceGO.transform.up;
+                }
+
+                addedIDs.Add(piece.InstanceID);
+
+                ShipGenerator.BuildConnectedPieces
+                    (piece, pieceGO.transform, ship);
+            }
+        }
+
+        #endregion
 
         #endregion
     }
--- Artificer/Assets/Scripts/System/AssetPreloader.cs
+++ Artificer/Assets/Scripts/System/AssetPreloader.cs
@@ -2,11 +2,13 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 // Artificer
 using Data.Shared;
 using Data.Space;
 using Data.Space.DataImporter;
 using Data.Space.Library;
+using UnityEngine.Networking;
 
 [RequireComponent(typeof(SystemAttributes))]
 public class AssetPreloader : MonoBehaviour
@@ -19,6 +21,7 @@ public class AssetPreloader : MonoBehaviour
         PreloadShips();
         PreloadItemAssets();
         PreloadFactions();
+        AddShipComponentsToNetwork();
     }
     
     private void PreloadShips()
@@ -53,6 +56,36 @@ public class AssetPreloader : MonoBehaviour
         _att.AILibrary = new AITemplateLibrary();
 
         AgentDataImporter.BuildTemplates(_att.AILibrary);
+    }
+
+    /// <summary>
+    /// Adds all our ship components in their 
+    /// directories to the network spawn list
+    /// automates the process
+    /// </summary>
+    private void AddShipComponentsToNetwork()
+    {
+        // Populate the top tab bar with component types
+        // Retreive all the directories
+        TextAsset ShipKey = Resources.Load("Space/Keys/ship_key",
+                                 typeof(TextAsset)) as TextAsset;
+
+        // Base Directory of ship components
+        string dir = "Space/Ships/{0}/";
+
+        // Loop through each component and add to the 
+        // Server spawn prefabs
+        foreach (string category in ShipKey.text.Split(","[0]))
+        {
+            // Load up all prefabs of that type
+            GameObject[] components = Resources.LoadAll
+                (string.Format(dir, category), typeof(GameObject))
+                .Cast<GameObject>().ToArray(); ;
+
+            // Add them to the network spawn prefab list
+            foreach (GameObject component in components)
+                ClientScene.RegisterPrefab(component);
+        }
     }
 }
 
